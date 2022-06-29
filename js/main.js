99f27569287d316e4688907875f9adca5978722b
Александр Кунгуров <i@axlmonk.ru>
2022-06-29 12:58:16 +0500

    Исправил замечания, убрал проверку

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ function getRandomInteger(from, before) {
   from = Math.ceil(from);
   before = Math.floor(before);
 
-  if (before <= from) {
-    return 'Ошибка! Максимальное значение не может ' +
-           'быть меньше значения минимального числа.';
-  }
   return Math.abs(Math.floor(Math.random() * (before - from + 1)) + from);
 }
 getRandomInteger(1, 100);
