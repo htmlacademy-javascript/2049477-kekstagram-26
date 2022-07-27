5f287078880b6a2482f27da4eb0d3a5d312d955a
Александр Кунгуров <i@axlmonk.ru>
2022-07-27 19:03:54 +0500

    Правки по замечаниям

--- js/scale.js
+++ js/scale.js
@@ -3,11 +3,14 @@ const scaleControlBigger = document.querySelector('.scale__control--bigger');
 const scaleControlValue = document.querySelector('.scale__control--value');
 const uploadImage = document.querySelector('.img-upload__preview img');
 
+const MAX_VALUE = 100;
+const MIN_VALUE = 25;
+
 const getScaleValue = () => parseInt(scaleControlValue.value, 10);
 
 const setScaleValue = (value) => {
-  let item = Math.max(value, 25);
-  item = Math.min(100, item);
+  let item = Math.max(value, MIN_VALUE);
+  item = Math.min(MAX_VALUE, item);
   scaleControlValue.value = `${item}%`;
   uploadImage.style.transform = `scale(${parseFloat(item/100)})`;
 };
